e31b9b0d811770e7e05b0c967d40bfa625c20e5d
Nate <natmartin@gmail.com>
2018-09-23 21:38:17 -0700

    added process highlights in mounted()

--- src/HighlightableInput.vue
+++ src/HighlightableInput.vue
@@ -54,6 +54,7 @@ export default {
   mounted () {
       if (this.fireOnEnabled)
         this.$el.addEventListener(this.fireOn, this.handleChange)
+      this.processHighlights()
   },
 
   watch: {
